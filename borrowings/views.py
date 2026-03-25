54812192c8cd7b3e235ab885a9c6bf8c6eb95f79
Maksym Pakula <gargantadoto@gmail.com>
2026-03-26 01:48:17 +0200

    Add creating to Borrowing viewset

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,17 +1,34 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingReadSerializer
+from borrowings.serializers import (
+    BorrowingReadSerializer,
+    BorrowingCreateSerializer
+)
 
 
 class BorrowingsViewSet(
-    viewsets.ReadOnlyModelViewSet
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingReadSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        if self.request.user.is_staff:
-            return self.queryset.filter(expected_return_date__isnull=True)
+        user = self.request.user
+        if not user.is_staff:
+            return self.queryset.filter(user=user)
 
-        return self.queryset.filter(user=self.request.user)
+        return self.queryset
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
