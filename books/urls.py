6465452abe79893603f1470f268572a9b57d83a4
Maksym Pakula <gargantadoto@gmail.com>
2026-03-25 16:14:54 +0200

    Add Book endpoints

---
+++ books/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from books.views import BooksViewSet
+
+router = routers.DefaultRouter()
+
+router.register("books", BooksViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "books"
