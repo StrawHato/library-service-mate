b430cc17391e897953321dc06bf7f131a5e5d13c
Maksym Pakula <gargantadoto@gmail.com>
2026-03-25 19:27:18 +0200

    Add Borrowings Read only viewset

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingReadSerializer
+
+
+class BorrowingsViewSet(
+    viewsets.ReadOnlyModelViewSet
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReadSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return self.queryset.filter(expected_return_date__isnull=True)
+
+        return self.queryset.filter(user=self.request.user)
