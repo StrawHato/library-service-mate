0b316b77ca96f7f6cf2c6d06c1aecf55b92a7c0b
Maksym Pakula <gargantadoto@gmail.com>
2026-03-25 15:25:00 +0200

    Add read_only field is_staff

--- user/serializers.py
+++ user/serializers.py
@@ -5,7 +5,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ["id", "email", "first_name", "last_name", "password"]
+        fields = ["id", "email", "first_name", "last_name", "is_staff", "password"]
+        read_only_fields = ["id", "is_staff"]
         extra_kwargs = {
             "password": {
                 "write_only": True,
