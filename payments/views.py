6221c00c8a8fa0e444c53381550824ec6fca2606
Maksym Pakula <gargantadoto@gmail.com>
2026-03-26 19:33:02 +0200

    Add Read Only Payments viewset

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentsViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Payment.objects.select_related("borrowing")
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        user = self.request.user
+
+        if not user.is_staff:
+            queryset = queryset.filter(borrowing__user__id=user.id)
+
+        return queryset
