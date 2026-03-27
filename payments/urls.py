543885ac2c9b699e2bf48971825e03e7d905f143
Maksym Pakula <gargantadoto@gmail.com>
2026-03-27 04:07:28 +0200

    Add cancel endpoint

--- payments/urls.py
+++ payments/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from payments.views import (
     PaymentsViewSet,
     SuccessView,
+    CancelView,
 )
 
 router = DefaultRouter()
@@ -11,6 +12,7 @@ router.register("", PaymentsViewSet, basename="payments")
 
 urlpatterns = [
     path("success/", SuccessView.as_view(), name="success"),
+    path("cancel/", CancelView.as_view(), name="cancel"),
     path("", include(router.urls)),
 ]
 
