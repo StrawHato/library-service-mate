ff97783e1e4c9712d08195b47f14b41f5a20fed9
Maksym Pakula <gargantadoto@gmail.com>
2026-03-27 04:06:43 +0200

    Add success endpoint

--- payments/urls.py
+++ payments/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from payments.views import PaymentsViewSet
+from payments.views import (
+    PaymentsViewSet,
+    SuccessView,
+)
 
 router = DefaultRouter()
 router.register("", PaymentsViewSet, basename="payments")
 
 urlpatterns = [
+    path("success/", SuccessView.as_view(), name="success"),
     path("", include(router.urls)),
 ]
 
