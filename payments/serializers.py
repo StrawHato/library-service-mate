acfa1a8b6fa43ba73c4b6fc68e4d9ce66e17c47d
Maksym Pakula <gargantadoto@gmail.com>
2026-03-27 03:33:04 +0200

    Add user_id field to Payment serializer

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
-from payments.models import Payment, calculate_money
+from payments.models import Payment
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(read_only=True, source="borrowing.user.id")
+
     class Meta:
         model = Payment
         fields = (
             "id", "status", "type", "borrowing",
-            "session_url", "session_id", "money_to_pay"
+            "session_url", "session_id", "money_to_pay", "user_id"
         )
