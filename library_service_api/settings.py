a2cb8a15e9e71f242608f9480cc43f1c323a38eb
Maksym Pakula <gargantadoto@gmail.com>
2026-03-27 22:46:46 +0200

    Migrate to PostgreSQL

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "library_service_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
